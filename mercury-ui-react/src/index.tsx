52291ed375580fa6f0dca573eb539d3ac5954416
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-06-24 20:29:48 -0600

    remove strict mode

--- mercury-ui-react/src/index.tsx
+++ mercury-ui-react/src/index.tsx
@@ -9,11 +9,9 @@ import { store } from "./redux/store"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 )
 
 // Verify that Neutralinojs is working
